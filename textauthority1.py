5197425d4a0cb849f5996d9475a3a3ccaef67082
tialaron <30555530+tialaron@users.noreply.github.com>
2022-05-17 23:11:06 +0300

    Update textauthority1.py

--- textauthority1.py
+++ textauthority1.py
@@ -14,12 +14,12 @@ from PIL import Image
 
 className = ["О. Генри", "Стругацкие", "Булгаков", "Клиффорд_Саймак", "Макс Фрай", "Брэдберри"] # Объявляем интересующие нас классы
 main_dir_t1 = 'H:\Pythonprojects\\textauthor1\\venv\content'
-model01 = load_model('H:\Pythonprojects\\textauthor1\\venv\model_author_all.h5')
+model01 = load_model('app/model_author_all.h5')
 # loading
-with open('H:\Pythonprojects\\textauthor1\\venv\\tokenizer.pickle', 'rb') as handle:
+with open('app/tokenizer.pickle', 'rb') as handle:
     tokenizer2 = pickle.load(handle)
 
-image_path = 'H:\Pythonprojects\\textauthor1\\venv\\realtrack1.jpg'
+image_path = 'app/realtrack1.jpg'
 #main_dir_t2 = '/content/class2/'
 
 newTest = []
